e6e169337fe6b41d07e5c9b4c2ae17ca6ca04ebd
agrahafiz13 <agrahafiz2@gmail.com>
2026-03-22 20:52:58 +0700

    membuat inisiasi firebase dengan service account credentials

--- config/firebase.go
+++ config/firebase.go
@@ -3,7 +3,19 @@ import (
 	"context"
 	"log"
 	"os"
+	
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
 	"google.golang.org/api/option"
 )
+
+// FirebaseAuth adalah instance Firebase Auth yang dipakai untuk verify token
+	var FirebaseAuth *auth.Client
+	func InitFirebase() {
+	credPath := os.Getenv("FIREBASE_CREDENTIALS_PATH")
+// Inisialisasi Firebase App dengan service account credentials
+	opt := option.WithCredentialsFile(credPath)
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+	log.Fatalf("Gagal init Firebase: %v", err)
+}
