f72df36df77cff73fde656358e6a00e8fc3f35a2
agrahafiz13 <agrahafiz2@gmail.com>
2026-03-22 20:36:55 +0700

    init database

--- config/database.go
+++ config/database.go
@@ -4,8 +4,21 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"https://github.com/agrahafiz13/gin-firebase-backend"
+
+	"github.com/agrahafiz13/gin-firebase-backend/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
+
+// DB adalah instance GORM global yang dipakai di seluruh aplikasi
+var DB *gorm.DB
+
+func InitDatabase() {
+	// Ambil konfigurasi dari environment variables
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	}
