7684ea9c381824afdd5a17cc01f298770fd36abe
agrahafiz13 <agrahafiz2@gmail.com>
2026-03-28 14:32:53 +0700

    fungsi find by email dan firebase uid

--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -10,3 +10,20 @@ type UserRepository struct{}
 func NewUserRepository() *UserRepository {
 	return &UserRepository{}
 }
+
+// FindByFirebaseUID mencari user berdasarkan Firebase UID
+func (r *UserRepository) FindByFirebaseUID(uid string) (*models.User, error) {
+	var user models.User
+	result := config.DB.Where("firebase_uid = ?", uid).First(&user)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &user, nil
+}
+
+// FindByEmail mencari user berdasarkan email
+func (r *UserRepository) FindByEmail(email string) (*models.User, error) {
+	var user models.User
+	result := config.DB.Where("email = ?", email).First(&user)
+	return &user, result.Error
+}
